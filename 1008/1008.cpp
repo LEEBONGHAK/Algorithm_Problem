0cc19385888e86682310ae8b405b482bbb690009
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-23 16:41:49 +0900

    1008

---
+++ 1008/1008.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double x, y;
+
+    cin >> x;
+    cin >> y;
+    cout << setprecision(12);
+    cout << x / y << endl;
+
+    return 0;
+}
