cfa9c78fb371875768b4f7ddf5ba61e1bd4f38a3
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-25 19:21:37 +0900

    1152

---
+++ 1152/1152.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string str;
+    int result = 0;
+
+    while (cin >> str) {
+        result++;
+    }
+
+    cout << result;
+
+    return 0;
+}
