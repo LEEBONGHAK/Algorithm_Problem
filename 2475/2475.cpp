dbab45e095a516f2946cb6e978181c8240709d8a
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:08:51 +0900

    2475

---
+++ 2475/2475.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    int result = 0;
+
+    cin >> a >> b >> c >> d >> e;
+
+    result = pow(a, 2) + pow(b, 2) + pow(c, 2) + pow(d, 2) + pow(e, 2);
+    result %= 10;
+
+    cout << result << endl;
+
+    return 0;
+}
