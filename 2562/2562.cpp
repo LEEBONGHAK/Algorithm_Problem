298ce7a7487f1071f451ac59e04242f67b3d67d9
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:13:35 +0900

    2562

---
+++ 2562/2562.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int result = -1;
+    int n, result_count;
+
+    for (int i = 0; i < 9; i++) {
+        cin >> n;
+        if (n > result) {
+            result = n;
+            result_count = i;
+        }
+    }
+
+    cout << result << endl;
+    cout << result_count + 1 << endl;
+
+    return 0;
+}
