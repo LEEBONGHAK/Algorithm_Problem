5b9a3ef66c422b1020360dcda6b5e222cd5abd68
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 15:38:45 +0900

    8958 OX퀴즈

---
+++ 8958/8958.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, result, score;
+    string str;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        result = 0;
+        score = 0;
+
+        cin >> str;
+        for (int j = 0; j < str.length(); j++) {
+            if (str[j] == 'X') {
+                score = 0;
+            }
+            else if ((j >= 1) && (str[j] == 'O') && (str[j] == str[j - 1])) {
+                score++;
+            }
+            else {
+                score = 1;
+            }
+            result += score;
+        }
+        cout << result << endl;
+    }
+
+    return 0;
+}
