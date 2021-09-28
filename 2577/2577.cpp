65ba392a2294f18047d77ebfcb054f775cfdc3bf
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:19:07 +0900

    2577

---
+++ 2577/2577.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    string temp;
+    string numbers = "0123456789";
+    int n_num[10] = { 0, };
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    temp = to_string(a * b * c);
+
+    for (int i = 0; i < temp.length(); i++) {
+        n_num[numbers.find(temp[i])]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", n_num[i]);
+    }
+
+    return 0;
+}
