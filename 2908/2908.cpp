8bc03dedf88d100f766cdb22447589532f755207
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 15:02:28 +0900

    2908

---
+++ 2908/2908.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+    swap(str1[0], str1[2]);
+    swap(str2[0], str2[2]);
+
+    if (stoi(str1) > stoi(str2)) {
+        cout << stoi(str1) << endl;
+    }
+    else {
+        cout << stoi(str2) << endl;
+    }
+
+    return 0;
+}
