39f1a7d0dcd971fced883fc1b323000094e08d2c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:02:51 +0900

    2439

---
+++ 2439/2439.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = n - i - 1; j > 0; j--) {
+            cout << ' ';
+        }
+        for (int k = 0; k < i + 1; k++) {
+            cout << "*";
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
