36371f17586cc03b4ec00f3b51c51f8191a4ced7
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 15:32:57 +0900

    3052

---
+++ 3052/3052.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int input, result = 0;
+	int arr[42] = { 0, };
+
+	for (int i = 0; i < 10; i++) {
+		cin >> input;
+
+		arr[input % 42]++;
+		if (arr[input % 42] == 1) {
+			result++;
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
