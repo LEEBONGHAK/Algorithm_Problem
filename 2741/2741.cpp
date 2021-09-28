575454967ce48595dd41af38cf7ad895045a8564
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:44:16 +0900

    2741

---
+++ 2741/2741.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
