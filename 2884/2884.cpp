c096e21f3fd173a1a92db7515b2cbb884c6a0fb6
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:58:23 +0900

    2884

---
+++ 2884/2884.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int hour, minute;
+    cin >> hour >> minute;
+
+    if ((minute - 45) < 0) {
+        hour -= 1;
+        if (hour < 0) {
+            hour = 23;
+        }
+        minute = 60 - (45 - minute);
+        cout << hour << " " << minute << endl;
+    }
+    else {
+        cout << hour << " " << minute - 45 << endl;
+    }
+
+    return 0;
+}
