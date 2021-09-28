a7e82dee5f4dec554f0e7c6332772184839596fa
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:53:52 +0900

    2753

---
+++ 2753/2753.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+
+	cin >> a;
+	if (((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0)) {
+		printf("1\n");
+	}
+	else {
+		printf("0\n");
+	}
+
+	return 0;
+}
