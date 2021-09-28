41a8a39979f3c5329a2b8e6445c472666ee821af
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 14:33:59 +0900

    2675

---
+++ 2675/2675.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T, R;
+	string S;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> R >> S;
+		for (int j = 0; j < S.length(); j++) {
+			for (int k = 0; k < R; k++) {
+				cout << S[j];
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
