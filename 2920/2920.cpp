de0f96e86077f042e92e50a4816cfb7b6fb33084
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-28 15:06:06 +0900

    2920 음계

---
+++ 2920/2920.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+
+    getline(cin, str);
+    if (str == "1 2 3 4 5 6 7 8") {
+        cout << "ascending\n";
+    }
+    else if (str == "8 7 6 5 4 3 2 1") {
+        cout << "descending\n";
+    }
+    else {
+        cout << "mixed\n";
+    }
+
+    return 0;
+}
