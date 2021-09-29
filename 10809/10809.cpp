0ae2c00601ad9cff1921d30cd2d5f08c7a7c52d2
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 12:33:28 +0900

    10809

---
+++ 10809/10809.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int index_str[26];
+    fill_n(index_str, 26, -1);
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    string str;
+
+    cin >> str;
+    for (int i = 0; i < str.length(); i++) {
+        if (index_str[alphabet.find(str[i])] == -1) {
+            index_str[alphabet.find(str[i])] = i;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%d ", index_str[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
