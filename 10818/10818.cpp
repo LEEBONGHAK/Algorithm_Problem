80dd2c03fa5c8516a95bb4195e73bb3d66de0e9c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 12:38:45 +0900

    10818

---
+++ 10818/10818.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    int max = -1000000;
+    int min = 1000000;
+    int compare;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> compare;
+        if (compare > max) {
+            max = compare;
+        }
+        if (compare < min) {
+            min = compare;
+        }
+    }
+
+    cout << min << " " << max << endl;
+
+    return 0;
+}
