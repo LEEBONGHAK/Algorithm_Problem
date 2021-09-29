d35474d9caafc6ab9d5bd3376ae8f88261b2aeef
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 13:09:41 +0900

    10998

---
+++ 10998/10998.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A, B;
+
+    cin >> A >> B;
+    cout << A * B << endl;
+
+    return 0;
+}
