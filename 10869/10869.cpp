6fcbc4e075a0340e4016c7559bbbb7a6fbb5686d
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 12:43:29 +0900

    10869

---
+++ 10869/10869.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A, B;
+
+	cin >> A >> B;
+
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+
+	return 0;
+}
