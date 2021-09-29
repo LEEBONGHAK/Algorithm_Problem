12a184b71b62079892c9f84f9911f038de45df5b
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 13:14:09 +0900

    11654

---
+++ 11654/11654.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char a;
+
+    cin >> a;
+    cout << int(a) << endl;
+
+    return 0;
+}
