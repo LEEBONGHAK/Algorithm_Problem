748534b91e5ab6468c6876678fbfbf04132c3559
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 13:19:55 +0900

    11720

---
+++ 11720/11720.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, result = 0;
+    string str;
+
+    cin >> n;
+    cin >> str;
+    for (int i = 0; i < n; i++) {
+        result += int(str[i]) - 48;
+    }
+
+    cout << result << endl;;
+
+    return 0;
+}
