757d54d9bf2aa303c8256a7450bc7476d539c54a
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 12:50:34 +0900

    10871

---
+++ 10871/10871.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);	// 동기화 해제
+	cin.tie(NULL);	// 묶음 해제
+
+	int N, X, c;
+	cin >> N >> X;
+
+	for (int i = 0; i < N; i++) {
+		cin >> c;
+		if (c < X) {
+			cout << c << " ";
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
