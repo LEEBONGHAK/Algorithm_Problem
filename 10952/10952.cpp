5c7f65432e4b2d36a76be49dbe3fb54bac56de5b
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-29 13:05:35 +0900

    10952

---
+++ 10952/10952.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+
+    while (cin >> a >> b) {
+        if (a == 0) {
+            break;
+        }
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
