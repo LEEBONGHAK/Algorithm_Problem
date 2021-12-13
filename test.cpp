f3ee5c42d53f636143dabce0951ce43ce2435125
octo <dlqhdgkr456@gmail.com>
2021-12-14 00:02:54 +0900

    15663

--- test.cpp
+++ test.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 
-#define MAX 8
 #define endl '\n'
 using namespace std;
 
 int n, m;
-int tmp[MAX], arr[MAX];
-bool check[MAX];
+int tmp[9], arr[9];
+int check[100001];
 
 void printCase(int pos)
 {
@@ -21,11 +20,13 @@ void printCase(int pos)
 
     for (int i = 0; i < n; i++)
     {
-        if (pos == 0) arr[pos] = tmp[i];
-        else if (arr[pos - 1] <= tmp[i]) arr[pos] = tmp[i];
+        if (i != 0 && tmp[i - 1] == tmp[i]) continue;
+        else if (check[tmp[i]] != 0) arr[pos] = tmp[i];
         else continue;
 
+        check[tmp[i]]--;
         printCase(pos + 1);
+        check[tmp[i]]++;
     }
 }
 
@@ -36,7 +37,10 @@ int main()
 
     cin >> n >> m;
     for (int i = 0; i < n; i++)
+    {
         cin >> tmp[i];
+        check[tmp[i]]++;
+    }
 
     sort(tmp, tmp + n);
     printCase(0);
