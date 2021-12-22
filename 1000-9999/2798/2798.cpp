0fd0958c9b909b910e751f370e1b0a542fe6369b
octo <dlqhdgkr456@gmail.com>
2021-12-22 16:38:27 +0900

    2798

---
+++ 1000-9999/2798/2798.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+
+int n, m, sums, close = 0;
+int tmp[3];
+int arr[101];
+bool check[101];
+
+void findResult(int pos)
+{
+  if (pos == 3)
+  {
+    sums = 0;
+    for (int i = 0; i < 3; i++)
+      sums += tmp[i];
+
+    if (sums <= m && abs(close - m) > abs(sums - m))
+      close = sums;
+
+    return;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    if (!check[i])
+    {
+      check[i] = true;
+      tmp[pos] = arr[i];
+      findResult(pos + 1);
+      check[i] = false;
+    }
+  }
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  findResult(0);
+
+  cout << close << endl;
+
+  return 0;
+}
