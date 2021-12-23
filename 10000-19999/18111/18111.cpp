411daf4a2844591059ecc16f96116ac2eb29e449
octo <dlqhdgkr456@gmail.com>
2021-12-23 20:20:07 +0900

    18111

---
+++ 10000-19999/18111/18111.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int n, m, b, t, remain_block, r, r1, r2;
+int maxh = 0, minh = 257;
+int arr[501][501];
+set<pair<int, int>> s;
+
+void findResult(int element)
+{
+  t = 0;
+  remain_block = b;
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (element < arr[i][j]) // 블록 제거
+      {
+        t += 2 * (arr[i][j] - element);
+        remain_block += arr[i][j] - element;
+      }
+      else // 블록 추가
+      {
+        t += element - arr[i][j];
+        remain_block -= element - arr[i][j];
+      }
+    }
+  }
+
+  if (t < 0 || remain_block < 0)
+    return;
+
+  s.insert({t, element});
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n >> m >> b;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+    {
+      cin >> arr[i][j];
+      if (minh > arr[i][j])
+        minh = arr[i][j];
+      if (maxh < arr[i][j])
+        maxh = arr[i][j];
+    }
+
+  for (int i = minh; i <= maxh; i++)
+    findResult(i);
+
+  r1 = (*s.begin()).first;
+  r2 = (*s.begin()).second;
+  for (auto i = s.begin(); i != s.end(); i++)
+    if (r1 == (*i).first)
+      r2 = (*i).second;
+
+  cout << r1 << " " << r2 << endl;
+
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int n, m, b, t, remain_block, r, r1, r2;
+int maxh = 0, minh = 257;
 int arr[501][501];
 set<pair<int, int>> s;
 
@@ -16,20 +17,20 @@ void findResult(int element)
     {
         for (int j = 0; j < m; j++)
         {
-            if (element < arr[i][j])
+            if (element < arr[i][j])    // 블록 제거
             {
-                t += arr[i][j] - element;
-                remain_block -= arr[i][j] - element;
+                t += 2 * (arr[i][j] - element);
+                remain_block += arr[i][j] - element;
             }
-            else
+            else    // 블록 추가
             {
-                t += 2 * (element - arr[i][j]);
-                remain_block += element - arr[i][j];
+                t += element - arr[i][j];
+                remain_block -= element - arr[i][j];
             }
         }
     }
 
-    if (remain_block < 0) t += 2 * remain_block;
+    if (t < 0 || remain_block < 0) return;
 
     s.insert({t, element});
 }
@@ -42,31 +43,21 @@ int main()
     cin >> n >> m >> b;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
+        {
             cin >> arr[i][j];
+            if (minh > arr[i][j]) minh = arr[i][j];
+            if (maxh < arr[i][j]) maxh = arr[i][j];
+        }
 
-    cout << 1 << endl;
+    for (int i = minh; i <= maxh; i++)
+        findResult(i);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            findResult(arr[i][j]);
+    r1 = (*s.begin()).first;
+    r2 = (*s.begin()).second;
+    for(auto i = s.begin(); i != s.end(); i++)
+        if (r1 == (*i).first) r2 = (*i).second;
 
-    cout << 1 << endl;
-    cout << s.size() << endl;
-    cout << *s.begin() << " " << *s.end() << endl;
-    cout << 1 << endl;
-
-    // r1 = s.begin().first;
-    // r2 = s.begin().second;
-    // for (auto p: s)
-    // {
-    //     if (r1 == p.first)
-    //     {
-    //         r1 = p.first;
-    //         r2 = p.second;
-    //     }
-    // }
-
-    // cout << r1 << " " << r2 << endl;
+    cout << r1 << " " << r2 << endl;
 
     return 0;
 }
