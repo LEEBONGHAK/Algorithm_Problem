1e9ca28f6ce32c763d227a9c2e52e194c02958fb
octo <dlqhdgkr456@gmail.com>
2021-12-23 19:41:05 +0900

    edit 15665 and 15666's README.md

--- test.cpp
+++ test.cpp
@@ -1,45 +1,72 @@
 #include <bits/stdc++.h>
-
 #define endl '\n'
+
 using namespace std;
 
-int n, m, t, l, bm, r, result;
-long long sum = 0;
-vector<int> v;
+int n, m, b, t, remain_block, r, r1, r2;
+int arr[501][501];
+set<pair<int, int>> s;
+
+void findResult(int element)
+{
+    t = 0;
+    remain_block = b;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (element < arr[i][j])
+            {
+                t += arr[i][j] - element;
+                remain_block -= arr[i][j] - element;
+            }
+            else
+            {
+                t += 2 * (element - arr[i][j]);
+                remain_block += element - arr[i][j];
+            }
+        }
+    }
+
+    if (remain_block < 0) t += 2 * remain_block;
+
+    s.insert({t, element});
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> n >> m;
+    cin >> n >> m >> b;
     for (int i = 0; i < n; i++)
-    {
-        cin >> t;
-        v.push_back(t);
-    }
+        for (int j = 0; j < m; j++)
+            cin >> arr[i][j];
 
-    sort(v.begin(), v.end());
+    cout << 1 << endl;
 
-    l = 0;
-    r = v.back();
-    while (l <= r)
-    {
-        sum = 0;
-        bm = (l + r) / 2;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            findResult(arr[i][j]);
 
-        for (int i: v)
-            if (i > bm) sum += i - bm;
+    cout << 1 << endl;
+    cout << s.size() << endl;
+    cout << s.begin() << " " << s.end() << endl;
+    cout << 1 << endl;
 
-        if (sum >= m)
-        {
-            l = bm + 1;
-            result = bm;
-        }
-        else r = bm - 1;
-    }
+    // r1 = s.begin().first;
+    // r2 = s.begin().second;
+    // for (auto p: s)
+    // {
+    //     if (r1 == p.first)
+    //     {
+    //         r1 = p.first;
+    //         r2 = p.second;
+    //     }
+    // }
 
-    cout << result << endl;
+    // cout << r1 << " " << r2 << endl;
 
     return 0;
 }
