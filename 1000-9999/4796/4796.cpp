c87fb24a1556a14d1d3649560c5b2fc0d77fbecb
octo <dlqhdgkr456@gmail.com>
2022-01-01 22:05:32 +0900

    4796

---
+++ 1000-9999/4796/4796.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+
+int l, p, v;
+int i = 1;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  while (1)
+  {
+    cin >> l >> p >> v;
+    if (!l)
+      break;
+
+    if (v % p >= l)
+      cout << "Case " << i << ": " << (v / p) * l + l << endl;
+    else
+      cout << "Case " << i << ": " << (v / p) * l + v % p << endl;
+    i++;
+  }
+
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,41 +3,22 @@
 #define endl '\n'
 using namespace std;
 
-int t, n, tmp1, tmp2;
-vector<int> v;
-
-int fibonacci(int n)
-{
-    if (n == 0) return 0;
-    else if (n == 1) return 1;
-    else if (int(v.size()) > n) return v[n];
-    else 
-    {
-        tmp1 = fibonacci(n - 1) + fibonacci(n - 2);
-        v.push_back(tmp1);
-        return tmp1;
-    }
-}
+int l, p, v;
+int i = 1;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    v.push_back(0);
-    v.push_back(1);
-
-    cin >> t;
-    for (int i = 0; i < t; i++)
+    while(1)
     {
-        cin >> n;
+        cin >> l >> p >> v;
+        if (!l) break;
 
-        if (n == 0) cout << "1 0" << endl;
-        else 
-        {
-            tmp2 = fibonacci(n);
-            cout << v[n - 1] << " " << tmp2 << endl;
-        }
+        if (v % p >= l) cout << "Case " << i << ": " << (v / p) * l + l << endl;
+        else cout << "Case " << i << ": " << (v / p) * l + v % p << endl;
+        i++;
     }
 
     return 0;
