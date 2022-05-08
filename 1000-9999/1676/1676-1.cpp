9a2178a7aa9cf92e72a4ce387402534c4a60e725
octo <dlqhdgkr456@gmail.com>
2022-05-08 21:42:14 +0900

    1676

--- 1000-9999/1676/1676-1.cpp
+++ 1000-9999/1676/1676-1.cpp
@@ -5,14 +5,25 @@ using namespace std;
 
 int n, cnt;
 
+void count_five(int num)
+{
+    if (num % 5 != 0)
+        return;
+    if (num % 5 == 0)
+    {
+        cnt++;
+        count_five(num / 5);
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> n;
-    for (int i = 5; i <= n; i *= 5)
-        cnt += n / i;
+    for (int i = 1; i <= n; i++)
+        count_five(i);
 
     cout << cnt << endl;
 
