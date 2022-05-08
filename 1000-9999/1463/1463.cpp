d4c85535d2771b16cba6b2c2b6492bd2b589df50
octo <dlqhdgkr456@gmail.com>
2022-05-08 18:47:20 +0900

    1463

--- 1000-9999/1463/1463.cpp
+++ 1000-9999/1463/1463.cpp
@@ -12,8 +12,10 @@ void dp()
     {
         arr[i] = arr[i - 1] + 1;
 
-        if (i % 2 == 0) arr[i] = min(arr[i], arr[i / 2] + 1);
-        if (i % 3 == 0) arr[i] = min(arr[i], arr[i / 3] + 1);
+        if (i % 2 == 0)
+            arr[i] = min(arr[i], arr[i / 2] + 1);
+        if (i % 3 == 0)
+            arr[i] = min(arr[i], arr[i / 3] + 1);
     }
 }
 
